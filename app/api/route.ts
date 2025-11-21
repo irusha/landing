af31d32ab709875408e8b8c582c0b7856b9145eb
irusha <irusha@codegen.net>
2025-11-21 14:54:12 +0530

    REPZ:SCRUM-75: CHORE: Commented route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,4 @@
+/*
 import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 
@@ -31,3 +32,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(500).json({ success: false, error: err.message });
     }
 }
+*/
