b1d5cbba73dabd2a55c6bcbdf2ce4ed08f7c71ad
Piviru <piviru@codegen.net>
2025-11-27 00:23:52 +0530

    REPZ:SCRUM-74:ENHC: Build issue fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -84,7 +84,9 @@ const PillNavigation: React.FC<PillNavigationProps> = ({ links }) => {
                     key={link.name}
                     href={link.path}
                     // Ref assignment ensures the ref type is correct
-                    ref={(el: HTMLAnchorElement | null) => (linkRefs.current[link.name] = el)}
+                    ref={(el: HTMLAnchorElement | null) => {
+                        linkRefs.current[link.name] = el;
+                    }}
                     onClick={() => handleLinkClick(link.name)}
                     className={`
                         relative z-10 py-2 px-6 text-sm font-semibold whitespace-nowrap
